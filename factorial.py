062e18e082e4af71031afe86b7bf2e1441e7a8c2
hsinem <hsinembaskal@gmail.com>
2021-07-15 19:34:43 +0300

    Second Commit

---
+++ factorial.py
@@ -0,0 +1,16 @@
+## Factorial Program
+
+n = int(input("Number: "))
+
+factorial = 1
+
+if n < 0:
+   print("Invalid input")
+
+elif n == 0:
+   print("The factorial: 1")
+
+else:
+   for i in range(1,n + 1):
+       factorial = factorial*i
+   print("The factorial :",factorial)
---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+## Fibonacci Program
+i = int(input("Step:"))
+n1, n2 = 0, 1
+counter = 0
+
+if i > 0:
+    while counter < i:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        counter += 1
+        
+elif i == 1:
+   print(n1)
+
+else:
+   print("invalid input")
